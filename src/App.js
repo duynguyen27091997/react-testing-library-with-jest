403b9d0fa6d8a5e81c5bbd1b88cee042bd95beae
duynn <ngocduy@tuoitre.com.vn>
2021-02-25 15:37:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing Updated</h1>
+      <h1>Testing Updated 2</h1>
     </div>
   );
 }
---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import App from './App'
+
+afterEach(cleanup)
+
+test('should take a snapshot again', () => {
+    const { asFragment } = render(<App />)
+
+    expect(asFragment(<App />)).toMatchSnapshot()
+})
